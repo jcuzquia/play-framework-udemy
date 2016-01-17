471bb34d0b7557a00cb09b90b4691870c7a8e22f
Camilo Uzquiano <camilo@solarenergy.org>
2016-01-17 17:22:15 -0600

    Lecture 17 Create Form and Activate Database Functionality

--- consulting-services-inc/app/controllers/Services.java
+++ consulting-services-inc/app/controllers/Services.java
@@ -1,16 +1,20 @@
 package controllers;
 
+import models.Service;
+import play.data.Form;
 import play.mvc.Result;
 import views.html.services.info;
 
 public class Services extends play.mvc.Controller{
 
+	private static final Form<Service> sServiceForm = Form.form(Service.class);
 	public Result list(){
 		return TODO;
 	}
 	
 	public Result addService(){
-		return ok(info.render());
+		
+		return ok(info.render(sServiceForm));
 	}
 	
 	public Result save(){
