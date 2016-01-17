3ed0ce77964e0ddfd0120360f5e3e2c342525c07
Camilo Uzquiano <camilo@solarenergy.org>
2016-01-17 16:00:29 -0600

    Lesson 14

---
+++ consulting-services-inc/app/controllers/Services.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import play.mvc.Result;
+
+public class Services extends play.mvc.Controller{
+
+	public Result list(){
+		return TODO;
+	}
+	
+	public Result newService(){
+		return TODO;
+	}
+}
